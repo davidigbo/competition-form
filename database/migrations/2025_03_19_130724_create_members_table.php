fe278f0c79842ab9138bda93142941235d92008c
David Igbo <davidokpah396@gmail.com>
2025-03-19 14:20:38 +0100

    creare models and run migrations

---
+++ database/migrations/2025_03_19_130724_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->constrained()->onDelete('cascade');
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('phone_address');
+            $table->string('institution_name');
+            $table->string('department');
+            $table->string('level');
+            $table->string('matric_number');
+            $table->string('proof_documnent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
